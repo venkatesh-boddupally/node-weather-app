6df91e4cdee5530611b9fee334b83a1dbda31f06
venkatesg <venkatesh.boddupally@talentica.com>
2019-08-03 14:18:16 +0530

    Initial commit

---
+++ public/js/app.js
@@ -0,0 +1,27 @@
+console.log('Client side javascript file is loaded!')
+
+const weatherForm = document.querySelector('form')
+const search = document.querySelector('input')
+const messageOne = document.querySelector('#message-1')
+const messageTwo = document.querySelector('#message-2')
+
+weatherForm.addEventListener('submit', (e)=>{
+    e.preventDefault()
+    
+    messageOne.textContent = 'Loading....'
+    messageTwo.textContent = ''
+
+    const url = 'http://localhost:3000/weather?address=' + search.value
+    fetch(url).then((response)=>{
+    response.json().then((data)=>{
+        if(data.error){
+            messageOne.textContent = data.error
+        }
+        else{
+            messageOne.textContent = data.location
+            messageTwo.textContent = data.forecast
+        }
+        search.value = ''
+    })
+    })
+})
---
+++ src/utils/forecast.js
@@ -0,0 +1,16 @@
+const request = require('request');
+
+const forecast = (latitude, longitude, callback) => {
+    const url = 'https://api.darksky.net/forecast/070f33adb36dea26d0766e8604190925/'+ latitude + ',' + longitude;
+    request({url, json: true}, (error, {body}) => {
+        if(error){
+            callback('Unable to connect to weather service');
+        }else if(body.error){
+            callback('Unable to find location');
+        }else{
+            callback(undefined, body.daily.data[0].summary + ' It is currently ' + body.currently.temperature + ' degress out. There is a ' + body.currently.precipProbability + '% chance of rain.')
+        }
+    });
+}
+
+module.exports = forecast;
